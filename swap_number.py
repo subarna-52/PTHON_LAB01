5d18f288df69f01b4e1ae797a68963b0975e82d4
Subarna Paul <146241788+subarna-52@users.noreply.github.com>
2024-08-01 02:16:45 +0530

    Delete swap_number.py

--- swap_number.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Enter the first number (a): "))
-b = float(input("Enter the second number (b): "))
-
-temp = a
-a = b
-b = temp
-
-print(f"After swapping: a = {a}")
-print(f"After swapping: b = {b}")
