2c76d5ae74ab5ae7e4328272c53152d3e6ab3a36
Subarna Paul <146241788+subarna-52@users.noreply.github.com>
2024-08-01 02:15:51 +0530

    Delete fahrenheit_to_celcius.py

--- fahrenheit_to_celcius.py
+++
@@ -1,8 +0,0 @@
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5/9
-
-fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
-
-celsius = fahrenheit_to_celsius(fahrenheit)
-
-print(f"{fahrenheit}°F is equal to {celsius:.2f}°C.")
